707878ef1dd2bca38bf105b6f52ccd8714472344
rohitrana90 <74606190+rohitrana90@users.noreply.github.com>
2022-03-30 19:42:42 +0530

    Add files via upload

---
+++ RemoveFile.py
@@ -0,0 +1,12 @@
+import os
+import shutil
+import time
+
+path = " /Enter the path of the file to delete"
+days = 30
+
+seconds = time.time()
+
+if os.path.exists()(
+os.walk("enter the path of file")
+)
